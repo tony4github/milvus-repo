0958a99f7b5a4ae963f5ca0b5da3128dac394118
Tony Liu <tony_andrew@msn.com>
2022-01-19 11:18:54 -0500

    Replace Jetty runner with webapp-runner(tomcat v8.5)

--- swagger-inflector/editor.sh
+++ swagger-inflector/editor.sh
@@ -1,2 +1,11 @@
 #!/usr/bin/env bash
-java -Dconfig=editor/inflector.yaml -jar editor/jetty-runner.jar --port 8000 editor/swagger-editor.war
+#Jetty runner commented out 
+#java -Dconfig=editor/inflector.yaml -jar editor/jetty-runner.jar --port 8000 editor/swagger-editor.war
+
+#tomcat v8.5 runner locally.
+#Local ruuner folder '/Users/air2020-i7/milvus-workspace/webapp-runner/'
+#Local jar folder '/Users/air2020-i7/milvus-workspace/swagger.io/swagger-inflector/editor/'
+# 'swagger-editor.war' is basically identical to the one from 'https://github.com/swagger-api/swagger-inflector/tree/master/scripts/bin'
+air@Tony webapp-runner % pwd
+/Users/air2020-i7/milvus-workspace/webapp-runner
+air@Tony webapp-runner % java -jar assembly/target/webapp-runner.jar /Users/air2020-i7/milvus-workspace/swagger.io/swagger-inflector/editor/swagger-editor.war
