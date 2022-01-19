fb1af855fbc6808ea80ab7eed993cd270bf3814c
Tony Liu <tony_andrew@msn.com>
2022-01-19 11:40:44 -0500

    update info about webapp-runner

--- swagger-inflector/README.md
+++ swagger-inflector/README.md
@@ -3,3 +3,5 @@ swagger inflect blog via Swagger.io : https://swagger.io/blog/api-development/wr
 swagger inflector repo : https://github.com/swagger-api/swagger-inflector
 
 swagger inflector tutorial (simple and naive) : https://examples.javacodegeeks.com/swagger-inflector-tutorial/
+
+webapp-runner (By Jan 2022, tomcat v8.5) : https://github.com/heroku/webapp-runner
--- swagger-inflector/editor.sh
+++ swagger-inflector/editor.sh
@@ -2,7 +2,7 @@
 #Jetty runner commented out 
 #java -Dconfig=editor/inflector.yaml -jar editor/jetty-runner.jar --port 8000 editor/swagger-editor.war
 
-#tomcat v8.5 runner locally.
+#tomcat v8.5 runner built locally (webapp-runner.jar) - https://github.com/heroku/webapp-runner
 #Local ruuner folder '/Users/milvus-workspace/webapp-runner/'
 #Local jar folder '/Users/milvus-workspace/swagger.io/swagger-inflector/editor/'
 # 'swagger-editor.war' is basically identical to the one from 'https://github.com/swagger-api/swagger-inflector/tree/master/scripts/bin'
