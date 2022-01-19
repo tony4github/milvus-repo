2cc3672e14e607c095742d804a6f80e4d3b6d6f0
Tony Liu <tony_andrew@msn.com>
2022-01-19 11:37:33 -0500

    update folder structure

--- swagger-inflector/editor.sh
+++ swagger-inflector/editor.sh
@@ -3,9 +3,9 @@
 #java -Dconfig=editor/inflector.yaml -jar editor/jetty-runner.jar --port 8000 editor/swagger-editor.war
 
 #tomcat v8.5 runner locally.
-#Local ruuner folder '/Users/air2020-i7/milvus-workspace/webapp-runner/'
-#Local jar folder '/Users/air2020-i7/milvus-workspace/swagger.io/swagger-inflector/editor/'
+#Local ruuner folder '/Users/milvus-workspace/webapp-runner/'
+#Local jar folder '/Users/milvus-workspace/swagger.io/swagger-inflector/editor/'
 # 'swagger-editor.war' is basically identical to the one from 'https://github.com/swagger-api/swagger-inflector/tree/master/scripts/bin'
 air@Tony webapp-runner % pwd
-/Users/air2020-i7/milvus-workspace/webapp-runner
-air@Tony webapp-runner % java -jar assembly/target/webapp-runner.jar /Users/air2020-i7/milvus-workspace/swagger.io/swagger-inflector/editor/swagger-editor.war
+/Users/milvus-workspace/webapp-runner
+air@Tony webapp-runner % java -jar assembly/target/webapp-runner.jar /Users/milvus-workspace/swagger.io/swagger-inflector/editor/swagger-editor.war
