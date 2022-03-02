b2499cb4c4f593f28661f41cd97cb2bdbd4798c1
Tony Liu <tony_andrew@msn.com>
2022-03-02 16:26:34 -0500

    script updates on runner

--- swagger-inflector/editor.sh
+++ swagger-inflector/editor.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 #Jetty runner commented out 
-#java -Dconfig=editor/inflector.yaml -jar editor/jetty-runner.jar --port 8000 editor/swagger-editor.war
+java -Dconfig=editor/inflector.yaml -jar editor/jetty-runner.jar --port 8000 editor/swagger-editor.war
 
 #tomcat v8.5 runner built locally (webapp-runner.jar) - https://github.com/heroku/webapp-runner
 #Local ruuner folder '/Users/milvus-workspace/webapp-runner/'
 #Local jar folder '/Users/milvus-workspace/swagger.io/swagger-inflector/editor/'
 # 'swagger-editor.war' is basically identical to the one from 'https://github.com/swagger-api/swagger-inflector/tree/master/scripts/bin'
-air@Tony webapp-runner % pwd
-/Users/milvus-workspace/webapp-runner
-air@Tony webapp-runner % java -jar assembly/target/webapp-runner.jar /Users/milvus-workspace/swagger.io/swagger-inflector/editor/swagger-editor.war
+# '-Dconfig=editor/inflector.yaml' is required. And, i believe 'editor/' is a URL path, not a physical folder during assembling. 
+#air@Tony webapp-runner % pwd
+#/Users/milvus-workspace/webapp-runner
+#air@Tony webapp-runner % java -Dconfig=editor/inflector.yaml -jar assembly/target/webapp-runner.jar /Users/milvus-workspace/swagger.io/swagger-inflector/editor/swagger-editor.war
